e9c479100644a7cc19fa6153ccc7d5114ab45c6a
sayamapp <sayamapp@gmail.com>
2021-03-30 13:55:25 +0900

    0330 hiru2

--- 06/assembler/src/main.rs
+++ 06/assembler/src/main.rs
@@ -1,13 +1,13 @@
 mod parser;
 mod commands;
 use std::env;
-use crate::parser::Commands;
+use parser::Lines;
+
 
 fn main() {
     // let args: Vec<String> = env::args().collect();
     // let parser = Lines::new(args[1].to_string());
     // println!("{:?}", parser);
     let args: Vec<String> = env::args().collect();
-    let commands = Commands::new(args[1].to_string());
-    println!("{:?}", commands);
+    let lines = Lines::new(&args[1].to_string());
 }
