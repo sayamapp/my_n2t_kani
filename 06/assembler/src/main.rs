4e9de9b1d5d4261441cfb47ef571ba398840c7eb
sayamapp <sayamapp@gmail.com>
2021-03-31 13:23:08 +0900

    0331 13

--- 06/assembler/src/main.rs
+++ 06/assembler/src/main.rs
@@ -2,13 +2,22 @@ mod parser;
 mod code;
 
 use std::env;
+use std::fs::File;
+use std::io::{BufWriter, Write};
 use crate::parser::Lines;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let lines = Lines::new(&args[1].to_string());
     let bins = lines.to_binary();
+
+    let mut output = String::new();
     for bin in bins {
-        println!("{}", bin);
+        output = output + &bin + &"\n".to_string();
     }
+
+    let mut file = BufWriter::new(File::create("test.hack").unwrap());
+    write!(file, "{}", output).unwrap();
+    file.flush().unwrap();
+
 }
