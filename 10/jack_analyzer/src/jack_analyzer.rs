4cc8f9ddd0f0b5456a46d64e5c11f52e829dbfb2
sayamapp <sayamapp@gmail.com>
2021-04-28 21:12:43 +0900

    0428

--- 10/jack_analyzer/src/jack_analyzer.rs
+++ 10/jack_analyzer/src/jack_analyzer.rs
@@ -1,16 +1,68 @@
-use std::{char::ToLowercase, fs};
-use crate::jack_tokenizer::JackTokenizer;
+use std::{fs::{self, File}, io::{BufWriter, Write}, path::{Path, PathBuf}};
+use crate::jack_tokenizer::{JackTokenizer, Tokens};
+
 
 pub fn analyze(args: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let input_jack_pathes = read_args(args);
-    println!("{:?}", input_jack_pathes);
-    for jack_filepath in input_jack_pathes {
-        let content = fs::read_to_string(&jack_filepath)?;
+    let file_pathes = VecFilePath::new(args);
+    for i in 0..file_pathes.input_files.len() {
+        let content = fs::read_to_string(&file_pathes.input_files[i])?;
 
         let tokenizer = JackTokenizer::new(content);
+        let tokens = Tokens::new(tokenizer);
+        let vec_xml = tokens.get_xml();
+
+        let mut buf_writer = 
+            BufWriter::new(File::create(&file_pathes.output_files[i]).unwrap());
+        let xml = vec_xml.join("\n");
+        write!(buf_writer, "{}", xml).unwrap();
+        buf_writer.flush().unwrap();
 
     }
     Ok(())
+}
+
+#[derive(Debug)]
+struct VecFilePath {
+    input_files: Vec<PathBuf>,
+    output_files: Vec<PathBuf>,
+}
+impl VecFilePath {
+    fn new(args: &str) -> Self {
+        let mut input_files = Vec::new();
+        let mut output_files = Vec::new();
+
+        let mut path = PathBuf::from(args);
+        if let Some(file_name) = &path.file_name() {
+            if file_name.to_str().unwrap().contains(".jack") {
+                input_files.push(path.clone());
+                path.set_extension("xml");
+                output_files.push(path);
+            } else {
+                for dir_entry in fs::read_dir(path).unwrap() {
+                    if let Ok(d) = dir_entry {
+                        let mut path = d.path();
+                        if path.to_str().unwrap().contains(".jack") {
+                            input_files.push(path.clone());
+                            path.set_extension("xml");
+                            output_files.push(path);
+                        }
+                    }
+                }
+            }
+        }
+        VecFilePath {
+            input_files,
+            output_files,
+        }
+    }
+}
+
+#[test]
+fn test() {
+    let path1 = "./testcase/ArrayTest/Main.jack";
+    let path2 = "./testcase/Square";
+    println!("{:?}", VecFilePath::new(path1));
+    println!("{:?}", VecFilePath::new(path2));
 }
 
 fn read_args(path: &str) -> Vec<String> {
--- 10/jack_analyzer/src/main.rs
+++ 10/jack_analyzer/src/main.rs
@@ -1,9 +1,42 @@
 mod jack_analyzer;
 mod jack_tokenizer;
 
-use std::env;
+use std::{env, fs, path::PathBuf};
 fn main() {
     let args: Vec<String> = env::args().collect();
     jack_analyzer::analyze(&args[1]);
 }
 
+#[test]
+fn test() {
+    let path1 = "./testcase/ArrayTest/Main.jack";
+    let path2 = "./testcase/Square/";
+    let path3 = "./testcase/Square";
+
+    let mut buf = PathBuf::from(path3);
+
+    let mut input_files = Vec::new();
+    let mut output_files = Vec::new();
+
+    if !buf.file_name().unwrap().to_str().unwrap().contains(".jack") {
+        let file_pathes = fs::read_dir(buf).unwrap();
+        for dir_entry in file_pathes {
+            if let Ok(d) = dir_entry {
+                if d.path().to_str().unwrap().contains(".jack") {
+                    let input_path = d.path().clone();
+                    let mut output_path = d.path();
+                    output_path.set_file_name(format!(
+                        "{}{}",
+                        output_path.file_stem().unwrap().to_str().unwrap(),
+                        ".xml"
+                    ));
+                    input_files.push(input_path);
+                    output_files.push(output_path);
+                }
+            }
+        }
+    }
+
+    println!("{:?}", input_files);
+    println!("{:?}", output_files);
+}
